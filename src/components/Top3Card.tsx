181746ba8c6ae151269ba4fa67c9c3c4499ace28
bruno <brunoqueiroz612@gmail.com>
2023-02-07 08:53:47 -0300

    Add Top3Card

---
+++ src/components/Top3Card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Top3CardProps {
+  img: string;
+  title: string;
+  description: string;
+  numberList: string;
+}
+
+const Top3Card = ({ img, title, description, numberList }: Top3CardProps) => {
+  return (
+    <article className="flex gap-4">
+      <img src={img} alt="" className="w-[140px]" />
+      <div className="flex flex-col gap-2">
+        <span className="text-4xl font-bold text-grayish-blue">
+          {numberList}
+        </span>
+        <h2 className="font-bold">{title}</h2>
+        <p className="text-dark-grayish-blue">{description}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Top3Card;
