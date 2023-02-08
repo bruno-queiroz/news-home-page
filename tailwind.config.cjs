b2d777ebcdfeb1c131cf9de0341cad18ca63d5fe
bruno <brunoqueiroz612@gmail.com>
2023-02-08 08:37:36 -0300

    Add font to the homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      homePage: ["Inter", "sans-serif"],
+    },
     extend: {
       colors: {
         "soft-orange": "hsl(35, 77%, 62%)",
